e375e7deb4ffbea30f3ee98d4e7c22e49cb5cb76
Ann Catherine Jose <ann.jose@gmail.com>
2024-02-23 15:36:51 -0800

    add debounce to the search feature - to limit the number of keyboard events sent to the server. It uses the use-debounce library.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,14 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(searchTerm: string) {
+  const handleSearch = useDebouncedCallback((searchTerm: string) => {
     console.log(`search term from user: ${searchTerm}`);
 
     let params = new URLSearchParams(searchParams);
@@ -17,13 +18,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('query');
     }
-    
+
     const newUrl = `${pathname}?${params.toString()}`;
     console.log(`newUrl: ${newUrl}`);
 
     // replace the URL without a page reload
     replace(newUrl);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
