bbe9fa09f9cc2cf67f20af729bb2bd6dc8c97679
Ann Catherine Jose <ann.jose@gmail.com>
2024-02-29 15:11:07 -0800

    Handle the notFound error with the new not-found.tsx page.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,5 +1,6 @@
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import EditInvoiceForm from "@/app/ui/invoices/edit-form";
+import { notFound } from "next/navigation";
 
 export default async function Page({
     params
@@ -16,6 +17,10 @@ export default async function Page({
         fetchCustomers()
     ])
 
+    if(!invoice) {
+        console.error(`Not found invoice with id ${invoiceId}`);
+        notFound();
+    }
     return (
         <EditInvoiceForm invoice={invoice} customers={customers} />
     );
