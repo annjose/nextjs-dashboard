d40db42eb0d7b3eeedf7a49bffea5a82bbf9e5d6
Ann Catherine Jose <ann.jose@gmail.com>
2024-03-19 10:36:04 -0700

    Added metadata attributes through code (title, description, keywords, creator). Implemented metadata for favicon and opengraph image as file conventions. Used template inside the title metadata.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,19 @@ import '@/app/ui/global.css';
 
 import { inter } from '@/app/ui/fonts';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'A simple invoicing appl built using Next.js',
+  keywords: ['nextjs', 'learn', 'invoice'],
+  metadataBase: new URL('https://nextjs-dashboard-tutorial-nine-mu.vercel.app/'),
+  creator: 'Ann Catherine Jose'
+};
+
 export default function RootLayout({
   children,
 }: {
